469882cc0687fd5f4605c4f844163535df2271ec
yostc <chad.yost3@nissan-usa.com>
2025-03-28 11:42:45 -0500

    changeing for

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -32,6 +33,7 @@ if ingredients_list:
     ingredients_string = ' '
     for fruit_chosen in ingredients_list:
          ingredients_string += fruit_chosen + ' '
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")         
 
     #st.write(ingredients_string)
 
@@ -49,7 +51,6 @@ if ingredients_list:
 
        st.success('Your smoothie is ordered! '+ name_on_order, icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
+
+
 st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
