d84fbcb56ef1d848eb3375678bce8a7ce8cc9a97
yostc <chad.yost3@nissan-usa.com>
2025-03-28 11:44:11 -0500

    for2

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ if ingredients_list:
     ingredients_string = ' '
     for fruit_chosen in ingredients_list:
          ingredients_string += fruit_chosen + ' '
-         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")         
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")    
+         st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)     
 
     #st.write(ingredients_string)
 
@@ -53,4 +54,4 @@ if ingredients_list:
 
 
 
-st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
