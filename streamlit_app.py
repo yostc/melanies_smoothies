415553f9c04c878a75e6152b6893df7e07f95587
yostc <chad.yost3@nissan-usa.com>
2025-03-28 11:38:26 -0500

    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-st_df =st.dataframe(data=smoothiefroot_response.Json(), use_container_width=True)
+st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
