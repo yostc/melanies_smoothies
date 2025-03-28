7cb0803ba3d6fa3101c27ff5777856fa721e3e22
yostc <chad.yost3@nissan-usa.com>
2025-03-28 11:15:02 -0500

    Update streamlit_app.py
    changing get session to cnx.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ name_on_order = st.text_input("Name On Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
 
-session = get_active_session()
+cnx=st.connection("Snowflake")
+session=st.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
