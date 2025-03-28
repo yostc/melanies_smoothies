187070602f1c317e41fdf5b3049885e4f2a254fd
yostc <chad.yost3@nissan-usa.com>
2025-03-28 12:23:18 -0500

    changeing search_on variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
          st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
          st.subheader(fruit_chosen + ' Nutrition information')   
-         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen )    
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +search_on )    
          st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)     
 
     #st.write(ingredients_string)
